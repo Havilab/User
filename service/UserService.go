8791d95dba493b20904d66f522598b864838b5ee
Gopal Pandilla <gopal.pandilla1@t-mobile.com>
2024-01-07 08:14:09 +0530

    order save issue fixed

--- service/UserService.go
+++ service/UserService.go
@@ -73,11 +73,8 @@ func CreateOrder(c *fiber.Ctx) error {
 	for _, order := range orders {
 		order.OderId = id
 		order.CreateDate = time.Now().Format("02-01-2006 3:4:5 PM")
-		result := database.Db().Create(&order)
+		database.Db().Create(&order)
 
-		if result.RowsAffected > 1 {
-			return c.Status(500).JSON(fiber.Map{"status": "500", "message": "Something went wrong to create order..."})
-		}
 	}
 	return c.Status(fiber.StatusOK).JSON(fiber.Map{"status": fiber.StatusOK, "OrderId": id})
 }
